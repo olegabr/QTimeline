8c8244d8d83b543ca6bd03f4c844c4c2d7fab65a
Oleg Abrosimov <olegabr@yandex.ru>
2013-05-30 00:48:27 +0700

    The example is made usable

--- source/example/timeline.php
+++ source/example/timeline.php
@@ -4,10 +4,19 @@
 	class ExampleForm extends QForm {
 		/** @var QSimpleTable */
 		protected $pnlTimeline;
+		/**
+		 * @var QJqButton
+		 */
+		protected $btnDraw;
 
 		protected function Form_Create() {
-			// Define the DataGrid
 			$this->pnlTimeline = new QTimeline($this, 'QTimelineId');
+			$this->pnlTimeline->ShowNavigation = true;
+			$this->pnlTimeline->AxisOnTop = true;
+			
+			$this->btnDraw = new QJqButton($this);
+			$this->btnDraw->Text = QApplication::Translate("Draw events");
+			$this->btnDraw->AddAction(new QClickEvent, new QJavaScriptAction("drawVisualization();"));
 		}
 	}
 
--- source/example/timeline.tpl.php
+++ source/example/timeline.tpl.php
@@ -1,12 +1,66 @@
 <?php require(__DOCROOT__ . __EXAMPLES__ . '/includes/header.inc.php'); ?>
 <?php $this->RenderBegin(); ?>
+    <script type="text/javascript">
+        function drawVisualization() {
+            // Create a JSON data table
+            var data = [
+                {
+                    'start': new Date(2013,4,29),
+						'content': 'Conversation<br><img src="http://almende.github.io/chap-links-library/js/timeline/examples/img/comments-icon.png" style="width:32px; height:32px;">'
+                },
+                {
+                    'start': new Date(2013,4,29,23,0,0),
+                    'content': 'Mail from boss<br><img src="http://almende.github.io/chap-links-library/js/timeline/examples/img/mail-icon.png" style="width:32px; height:32px;">'
+                },
+                {
+                    'start': new Date(2013,4,30,16,0,0),
+                    'content': 'Report'
+                },
+                {
+                    'start': new Date(2013,5,1),
+                    'end': new Date(2013,6,3),
+                    'content': 'Traject A'
+                },
+                {
+                    'start': new Date(2013,5,2),
+                    'content': 'Memo<br><img src="http://almende.github.io/chap-links-library/js/timeline/examples/img/notes-edit-icon.png" style="width:48px; height:48px;">'
+                },
+                {
+                    'start': new Date(2013,5,3),
+                    'content': 'Phone call<br><img src="http://almende.github.io/chap-links-library/js/timeline/examples/img/Hardware-Mobile-Phone-icon.png" style="width:32px; height:32px;">'
+                },
+                {
+                    'start': new Date(2013,5,5),
+                    'end': new Date(2013,6,5),
+                    'content': 'Traject B'
+                },
+                {
+                    'start': new Date(2013,5,4,12,0,0),
+                    'content': 'Report<br><img src="http://almende.github.io/chap-links-library/js/timeline/examples/img/attachment-icon.png" style="width:32px; height:32px;">'
+                }
+            ];
 
-<div class="instructions">
-	<h1 class="instruction_title">Displaying a timeline with a QTimeline</h1>
-</div>
-<div style="margin-left: 100px">
+            // Draw our timeline with the created data and options
+			$j('#QTimelineId').timeline('draw', data);
+        }
+    </script>
+	
+	<div class="instructions">
+		<h1 class="instruction_title">Timeline JSON data</h1>
+		<p>
+			This demo shows how to feed the Timeline with JSON data.
+			It mimics the original example from here: <a href="http://almende.github.io/chap-links-library/js/timeline/examples/example17_json_data.html">Timeline JSON data</a>
+		</p>
+	</div>
+	
+	<?php $this->btnDraw->Render(); ?>
 	<?php $this->pnlTimeline->Render(); ?>
-</div>
+
+<!-- Information about where the used icons come from -->
+<p style="color:gray; font-size:10px; font-style:italic;">
+    Icons by <a href="http://dryicons.com" target="_blank" title="Aesthetica 2 Icons by DryIcons" style="color:gray;" >DryIcons</a>
+    and <a href="http://www.tpdkdesign.net" target="_blank" title="Refresh Cl Icons by TpdkDesign.net" style="color:gray;" >TpdkDesign.net</a>
+</p>
 
 <?php $this->RenderEnd(); ?>
 <?php require(__DOCROOT__ . __EXAMPLES__ . '/includes/footer.inc.php'); ?>
